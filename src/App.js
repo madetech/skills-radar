5a49702d3ce41515800cf70d315aedf30c36f5e7
Richard Race <1273965+richrace@users.noreply.github.com>
2025-01-14 12:52:22 +0000

    Rename the Skills Radar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import RadarChart from "./components/RadarChart";
-import { RadarProvider } from "./components/RadarProvider.js";
-import "./App.css";
 import {
-  Chart,
   ArcElement,
-  BarElement,
   BarController,
+  BarElement,
   CategoryScale,
-  LinearScale,
-  PointElement,
-  LineController,
-  RadialLinearScale,
-  LineElement,
-  RadarController,
-  Tooltip,
+  Chart,
   Filler,
   Legend,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  RadarController,
+  RadialLinearScale,
+  Tooltip,
 } from "chart.js";
+import "./App.css";
+import RadarChart from "./components/RadarChart";
+import { RadarProvider } from "./components/RadarProvider.js";
 Chart.register(
   ArcElement,
   BarElement,
@@ -36,7 +36,7 @@ Chart.register(
 function App() {
   return (
     <RadarProvider>
-      <h2 style={{ textAlign: "center" }}>Radar Chart</h2>
+      <h2 style={{ textAlign: "center" }}>Skills Radar</h2>
 
       <div className="App">
         <RadarChart />
